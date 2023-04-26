75c7fabf5dad6e8375e84a3f67b2b2697eb03328
Blake Lawson <blawson420@icloud.com>
2023-04-26 09:47:21 -0500

    Initial Commit

---
+++ MealMaster/MealMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealMasterApp.swift
+//  MealMaster
+//
+//  Created by Blake Lawson on 4/26/23.
+//
+
+import SwiftUI
+
+@main
+struct MealMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
